6a62a14868c1ac58ef99c7d161a29f2296d3b872
joceano <alves.joceano@gmail.com>
2021-04-25 17:42:00 -0300

    Implementado a busca de lançamentos vencidos

--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -4,5 +4,11 @@ import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
+
+    List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
+
 }
--- src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
+++ src/main/java/com/example/algamoney/api/repository/UsuarioRepository.java
@@ -3,10 +3,13 @@ package com.example.algamoney.api.repository;
 import com.example.algamoney.api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public Optional<Usuario> findByEmail(String email);
+
+    public List<Usuario> findByPermissoesDescricao(String permissaoDescricao);
 
 }
