8f045a5c1b9dac08571254c39cbc76fd0124e496
joceano <joceano@viasoft.com.br>
2021-04-26 08:29:25 -0300

    Implementado logs no agendamento do envio de e-mail

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -13,6 +13,8 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -31,6 +33,8 @@ public class LancamentoService {
 
     private static final String DESTINATARIOS = "ROLE_PESQUISAR_LANCAMENTO";
 
+    private static final Logger logger = LoggerFactory.getLogger(LancamentoService.class);
+
     @Autowired
     private PessoaRepository pessoaRepository;
 
@@ -46,13 +50,31 @@ public class LancamentoService {
     //@Scheduled(fixedDelay = 1000 * 5)
     @Scheduled(cron = "0 0 6 * * *")
     public void avisarSobreLancamentosVencidos() {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Preparando envio de e-mails de aviso de lançamentos vencidos");
+        }
+
         List<Lancamento> vencidos = lancamentoRepository
                 .findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
+
+        if (vencidos.isEmpty()) {
+            logger.info("Sem lançamentos vencidos para aviso;.");
+            return;
+        }
+
+        logger.info("Existem {} lançamentos vencidos.", vencidos.size());
 
         List<Usuario> destinatarios = usuarioRepository
                 .findByPermissoesDescricao(DESTINATARIOS);
 
+        if (destinatarios.isEmpty()) {
+            logger.warn("Existem lançamentos vencidos, mas o sistema não encontrou destinatários.");
+            return;
+        }
+
         mailer.avisarSobreLancamentosVencidos(vencidos, destinatarios);
+
+        logger.info("Envio de e-mail de aviso concluído.");
     }
 
     public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
