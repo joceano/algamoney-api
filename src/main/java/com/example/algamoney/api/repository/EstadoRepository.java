47588ad204fcc75a04b939487929181511a0a04a
joceano <alves.joceano@gmail.com>
2021-05-16 15:41:29 -0300

    Implementado a pesquisa de estados e cidades

---
+++ src/main/java/com/example/algamoney/api/repository/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.example.algamoney.api.repository;
+
+import com.example.algamoney.api.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
